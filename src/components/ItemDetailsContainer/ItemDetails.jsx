827cdbc7577db026a7936d094126564f73351209
AgustinR <sac_974@hotmail.com>
2022-10-05 13:17:20 -0300

    cambios entregable

--- src/components/ItemDetailsContainer/ItemDetails.jsx
+++ src/components/ItemDetailsContainer/ItemDetails.jsx
@@ -1,24 +1,26 @@
 import React, { useContext, useState } from "react";
-import ItemCount from "../ItemCount/ItemCount";
+import { Link } from "react-router-dom";
 import {CartContext} from "../Context/Context";
+import ItemCount from "../ItemCount/ItemCount";
 
 const ItemDetails = ({item}) => {
     const {addItem} = useContext(CartContext);
     const [counter, setCounter] = useState(0);
-    const onAdd = (item, counter) => {
-        setCounter(counter);
+
+    const onAdd = (counter) => {
+        setCounter(counter); 
         addItem(item, counter);
     }
 
     return (
-        <div className="row">
-            <div className="col-md-4 offset-md-4 text-center color_marron">
+        <div className="row mb-5">
+            <div className="col-md-6 offset-md-3 text-center color_marron">
                 <img src={"../images/" + item.imagen} className="img-fluid" alt={item.nombre} />
                 <h1>{item.nombre}</h1>
                 <h3>{item.destacado}</h3>
                 <p><b>${item.precio}</b></p>
                 <p>{item.descripcion}</p>
-                <ItemCount initial={1} stock={item.stock} onAdd={onAdd} counter={counter} setCounter={setCounter} item={item} />
+                {counter === 0 ? <ItemCount initial={1} stock={item.stock} onAdd={onAdd} /> : <Link to={"/cart"} className="btn fondo_naranja">Finalizar mi Compra</Link>}
             </div>
         </div>
     )
